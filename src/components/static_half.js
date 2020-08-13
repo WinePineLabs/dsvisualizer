f0d5365f8b150e0cda437851bc32a9ded1ba98ed
winepine Zz <basitsaeed1@gmail.com>
2020-08-13 11:47:05 -0700

    form handle

--- src/components/static_half.js
+++ src/components/static_half.js
@@ -2,7 +2,26 @@ import React from 'react'
 import './static_half_style.css'
 import Algos from './algos'
 export default class StaticHalf extends React.Component{
+    
+    state={
+        newItem:'',
+        data:'BinaryTree',
+        items:[],
+    }
+
+    inputchanges=event=>{
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+    structchange=event=>{
+        this.setState({
+
+        })
+    }
+
     render(){
+        console.log(this.state);
         return(
             <div className='staticDiv'>
                 <div className='sitename'>
@@ -12,14 +31,15 @@ export default class StaticHalf extends React.Component{
                 <div className='options'>
 
                     <div className='data'>
-                        <select name="Data Structure" id="">
-                            <option value="">Stacks</option>
-                            <option value="">BinaryTree</option>
-                            <option value="">Trie</option>
-                            <option value="">Heap</option>
+                        <select onChange={this.inputchanges} value={this.state.data} name="data" id="">
+                            <option>Stacks</option>
+                            <option>BinaryTree</option>
+                            <option>Trie</option>
+                            <option>Heap</option>
                         </select>
-                        <input value='Add Item' type="text" name='Add'/>
+                        <input placeholder='New Item' name='newItem' value={this.state.newItem} type="text" onChange={this.inputchanges}/>
                         <button>Add Item</button>
+                        <button>Reset</button>
                     </div>
                     
                     <Algos />
